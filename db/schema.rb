9d05c0f939aeb6001ce9673a833e47902b26f610
hshessar <hshessar@gmail.com>
2014-09-23 21:20:18 +0300

    Logging in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921064236) do
+ActiveRecord::Schema.define(version: 20140923171543) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20140921064236) do
     t.string   "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
