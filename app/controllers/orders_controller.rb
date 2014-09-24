e76bb66d04eb0e4c38f3e02f8cdd14c4c7d17e56
hshessar <hshessar@gmail.com>
2014-09-24 21:43:43 +0300

    Internationalization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,18 +4,13 @@ class OrdersController < ApplicationController
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
-  # GET /orders
-  # GET /orders.json
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
   end
 
-  # GET /orders/new
   def new
     if @cart.line_items.empty?
       redirect_to store_url, notice: "Your cart is empty"
@@ -25,12 +20,9 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
-  # GET /orders/1/edit
   def edit
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
@@ -41,17 +33,17 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice:
-          'Thank you for your order.' }
-        format.json { render action: 'show', status: :created, location: @order }
+          I18n.t('.thanks') }
+        format.json { render action: 'show', status: :created,
+          location: @order }
       else
         format.html { render action: 'new' }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
     respond_to do |format|
       if @order.update(order_params)
@@ -64,8 +56,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
     @order.destroy
     respond_to do |format|
@@ -75,12 +65,10 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:name, :adress, :email, :pay_type)
     end
